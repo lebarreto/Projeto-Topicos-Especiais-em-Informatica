ac9ee09cec9e936bd9159344e56f2814b293742a
leticialmb <leticiabarreto196@gmail.com>
2020-08-24 10:43:45 -0400

    initial commit

---
+++ src/main/java/br/gov/sp/fatec/projetotopicosespeciais/ProjetoTopicosEspeciaisApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetotopicosespeciais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTopicosEspeciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTopicosEspeciaisApplication.class, args);
+	}
+
+}
